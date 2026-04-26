ad4182d25646e2c865c3e62d10321b6b9db2ae0c
contatoatendimentovary-rgb <contatoatendimentovary@gmail.com>
2026-04-26 00:38:55 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,32 @@
-const CACHE_NAME = 'fpro-vfinal';
+const CACHE_NAME = 'financas-pro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
 
+// Instalação: Salva arquivos essenciais offline
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './style.css',
-                './script.js',
-                './manifest.json'
-            ]);
-        })
-    );
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Ativação: Limpa caches antigos
 self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
+  event.waitUntil(clients.claim());
 });
 
+// Fetch: Essencial para o Chrome liberar o botão de instalar
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
